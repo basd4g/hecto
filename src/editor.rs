93e0f41817333af9277654487968ace9a6aa1467
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-01-17 21:57:03 +0900

    Add to print 'Goodbye.' with quiting

--- src/editor.rs
+++ src/editor.rs
@@ -36,6 +36,9 @@ impl Editor {
   }
   fn refresh_screen(&self) -> Result<(), std::io::Error> {
     print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    if self.should_quit {
+      println!("Goodbye. \r");
+    }
     io::stdout().flush()
   }
 }
