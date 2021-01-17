ae8114484701b92fd717f5b0c60e81d4023068f0
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-01-17 14:28:09 +0900

    Read from stdin with raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
-use std::io::Read;
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+  let _stdout = stdout().into_raw_mode().unwrap();
   for b in io::stdin().bytes() {
     let c = b.unwrap() as char;
     println!("{}", c);
