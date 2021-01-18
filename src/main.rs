45363bea535de12d09453a8c0f9254b6a9e270de
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-01-18 18:20:41 +0900

    Add struct Document, struct Row

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod row;
+mod document;
 mod editor;
 mod terminal;
+pub use row::Row;
 use editor::Editor;
-pub use terminal::Terminal;
+pub use document::Document;
 pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
   Editor::default().run();
