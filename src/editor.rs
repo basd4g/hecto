c94860f4cce28574425c252648e478bce0c8b655
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-01-18 18:59:11 +0900

    Draw from file

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
 use crate::Document;
 use crate::Row;
 use crate::Terminal;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -34,11 +35,19 @@ impl Editor {
     }
   }
   pub fn default() -> Self {
+    let args: Vec<String> = env::args().collect();
+    let document = if args.len() > 1 {
+      let file_name = &args[1];
+      Document::open(&file_name).unwrap_or_default()
+    } else {
+      Document::default()
+    };
+
     Self{
       should_quit: false,
       terminal: Terminal::default().expect("Failed to initialize terminal"),
       cursor_position: Position::default(),
-      document: Document::open(),
+      document,
     }
   }
   fn process_keypress(&mut self) -> Result<(), std::io::Error> {
